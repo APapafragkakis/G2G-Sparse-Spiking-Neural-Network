afb92890afbb33599b5b551b063bbfde0920c67a
Alexandros Papafragakis <alexpapafragakis7@gmail.com>
2025-12-10 15:22:26 +0200

    s

--- run_pprime_experiments.bat
+++ run_pprime_experiments.bat
@@ -11,19 +11,19 @@ if not exist logs (
 
 echo.
 echo ---- RUN 1: p_inter = 0.00 ----
-python train.py --model index --epochs 10 --batch_size 128 --T 25 --p_inter 0.00 > logs/p_000.txt
+python src/train.py --model=index --epochs=10 --batch_size=128 --T=25 --p_inter=0.00 > logs/p_000.txt
 echo Log saved to logs/p_000.txt
 echo -----------------------------
 
 echo.
 echo ---- RUN 2: p_inter = 0.10 ----
-python train.py --model index --epochs 10 --batch_size 128 --T 25 --p_inter 0.10 > logs/p_010.txt
+python src/train.py --model=index --epochs=10 --batch_size=128 --T=25 --p_inter=0.10 > logs/p_010.txt
 echo Log saved to logs/p_010.txt
 echo -----------------------------
 
 echo.
 echo ---- RUN 3: p_inter = 0.25 ----
-python train.py --model index --epochs 10 --batch_size 128 --T 25 --p_inter 0.25 > logs/p_025.txt
+python src/train.py --model=index --epochs=10 --batch_size=128 --T=25 --p_inter=0.25 > logs/p_025.txt
 echo Log saved to logs/p_025.txt
 echo -----------------------------
 
